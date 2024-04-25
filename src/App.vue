479ea7d61f634ee1e4ca9d3da6f68f0c51d6fca2
mskim8333 <mskim8333@naver.com>
2024-04-25 12:46:18 +0900

    폰트 및 로그인 레이아웃 적용

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,27 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Pretendard, sans-serif;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.v-application {
+  font-family: 'Pretendard', sans-serif !important;
+}
+@font-face {
+  font-family:'Pretendard';
+  src: url('assets/font/Pretendard-Regular.ttf') format('truetype');
+  font-weight: 400;
+}
+@font-face {
+  font-family:'Pretendard';
+  src: url('assets/font/Pretendard-Light.ttf') format('truetype');
+  font-weight: 300;
+}
+@font-face {
+  font-family:'Pretendard';
+  src: url('assets/font/Pretendard-Bold.ttf') format('truetype');
+  font-weight: 700;
 }
 </style>
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -7,6 +7,25 @@
         <input v-model="token" placeholder="토큰값을 입력해주세요.">
       </div>
     </div>
+    <div class="ip_row">
+      <span>아이디</span>
+      <div>
+        <input v-model="id" placeholder="아이디를 입력해주세요.">
+      </div>
+    </div>
+    <div class="ip_row">
+      <span>비밀번호</span>
+      <div>
+        <input v-model="pass" placeholder="비밀번호를 입력해주세요.">
+      </div>
+    </div>
+    <div class="btn_wrap">
+      <button class="btn">로그인</button>
+    </div>
+    <div class="info">
+      <div class="bline">기기 고장 및 AS/사용법 문의</div>
+      <div class="rline">지구하다 고객센터 1234-1234</div>
+    </div>
   </div>
 </template>
 
@@ -21,23 +40,65 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.info {
+  position: fixed;
+  bottom: 35px;
+  width: 100%;
+}
+.info .bline {
+  font-size: 14px;
+  font-weight: 600;
+  color: #767676;
+  line-height: 26px;
+}
+.info .rline {
+  font-size: 14px;
+  color: #767676;
+  line-height: 26px;
+}
 img.logo {
-  margin: 120px 0 80px;
+  margin: 120px 0 100px;
   width: 50%;
 }
 .ip_row {
-  margin: 0 30px 10px;
+  margin: 0 30px 20px;
+  overflow: hidden;
 }
 .ip_row span {
   float: left;
   width: 20%;
+  padding: 10px 0;
+  font-size: 20px;
+  text-align: left;
+  color: #2C2C2C;
 }
 .ip_row div {
   float: right;
   width: 80%;
+  border-bottom: 2px solid #E4E4E4;
 }
 .ip_row div input {
   width: 100%;
+  padding: 10px;
+  border: 0px;
+  font-size: 20px;
+  color: #767676;
+  outline: none;
+}
+.ip_row div input::placeholder {
+  color: #B4B4B4;
+}
+.btn_wrap {
+  margin: 60px 26px;
+}
+.btn {
+  background-color: #1E64FF;
+  border-radius: 10px;
+  color: #FFFFFF;
+  font-size: 20px;
+  width: 100%;
+  height: 60px;
+  border: 0px;
 }
 h3 {
   margin: 40px 0 0;
