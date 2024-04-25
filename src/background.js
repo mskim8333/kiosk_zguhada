2611234ad2a5ea3e7f5cb3d6289c6e25f3f102d7
ysikkim <iysya@naver.com>
2024-04-25 11:45:16 +0900

    로고 업데이트

--- src/background.js
+++ src/background.js
@@ -13,8 +13,8 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 1100,
+    width: 600,
+    height: 800,
     autoHideMenuBar: true,
     webPreferences: {
       
