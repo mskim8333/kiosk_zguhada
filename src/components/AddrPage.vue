a728c231768c38cc8df9537cfc7b56554b0b9651
ysikkim <iysya@naver.com>
2024-05-11 18:17:39 +0900

    코드정리

--- src/components/AddrPage.vue
+++ src/components/AddrPage.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="addrpage">
-    <!-- 상단 로고 및 버튼 -->
     <div class="top_row">
       <router-link to="/">
         <img class="top_btn" alt="logo" src="../assets/home.png">
       </router-link>
     </div>
-    <!-- 타이틀 및 입력창 -->
     <h5>대형생활폐기물 배출을 위해</h5>
     <h3>배출하실 주소를 입력해주세요.</h3>
     <div class="ip_row">
@@ -14,16 +12,41 @@
         <input v-model="addr" maxlength="15" placeholder="주소를 입력해주세요.">
       </div>
     </div>
-    <!-- 한글 자판 -->
     <div class="keyboard">
       <div class="key_row">
-        <button v-for="key in keys" class="key_btn">{{ key }}</button>
+        <button class="key_btn">ㅂ</button>
+        <button class="key_btn">ㅈ</button>
+        <button class="key_btn">ㄷ</button>
+        <button class="key_btn">ㄱ</button>
+        <button class="key_btn">ㅅ</button>
+        <button class="key_btn">ㅛ</button>
+        <button class="key_btn">ㅕ</button>
+        <button class="key_btn">ㅑ</button>
+        <button class="key_btn">ㅐ</button>
+        <button class="key_btn">ㅔ</button>
       </div>
       <div class="key_row">
-        <button v-for="key in mCombo" class="key_btn">{{ key }}</button>
+        <button class="key_btn">ㅁ</button>
+        <button class="key_btn">ㄴ</button>
+        <button class="key_btn">ㅇ</button>
+        <button class="key_btn">ㄹ</button>
+        <button class="key_btn">ㅎ</button>
+        <button class="key_btn">ㅗ</button>
+        <button class="key_btn">ㅓ</button>
+        <button class="key_btn">ㅏ</button>
+        <button class="key_btn">ㅣ</button>
       </div>
       <div class="key_row">
-        <button v-for="key in jCombo" class="key_btn">{{ key }}</button>
+        <button class="key_btn key_icon">
+          <img src="../assets/upper.png" />
+        </button>
+        <button class="key_btn">ㅋ</button>
+        <button class="key_btn">ㅌ</button>
+        <button class="key_btn">ㅊ</button>
+        <button class="key_btn">ㅍ</button>
+        <button class="key_btn">ㅠ</button>
+        <button class="key_btn">ㅜ</button>
+        <button class="key_btn">ㅡ</button>
         <button class="key_btn key_icon">
           <img src="../assets/delete.png" />
         </button>
@@ -37,7 +60,6 @@
       </div>
     </div>
 
-    <!-- 하단 내용 -->
     <div class="bottom_content">
       <div class="icon_row">
         <span class="tip">Tip</span>
@@ -46,140 +68,123 @@
         아래와 같은 조합으로 검색을 하시면 더욱 정확한 결과가 검색됩니다.
       </div>
       <div class="info_box">
-        <div class="info_row" v-for="(info, index) in addressInfos" :key="index">
-          <span>{{ info.label }}</span>
-          <span>{{ info.example }}</span>
+        <div class="info_row">
+          · 도로명 + 건물명
+          <span>예)서초대로</span>
+        </div>
+        <div class="info_row">
+          · 동/읍/면/리 + 번지
+          <span>예)성수동 999-999</span>
+        </div>
+        <div class="info_row">
+          · 건물명/아파트명
+          <span>예)성수한라시트마벨리</span>
         </div>
       </div>
       <div class="btn_wrap">
         <router-link class="btn" to="/addr2">임시 다음페이지 이동</router-link>
       </div>
     </div>
+    
   </div>
 </template>
 
-<script>
-export default {
-  name: 'AddrPage',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      addr: '',
-      inputText: '',
-      chosungIndex: ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"],
-      jongsungIndex: ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"],
-      jComboIndex: ["ㄳ", "ㄵ", "ㄶ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅄ"],
-      jCombo: ["ㄱㅅ", "ㄴㅈ", "ㄴㅎ", "ㄹㄱ", "ㄹㅁ", "ㄹㅂ", "ㄹㅅ", "ㄹㅌ", "ㄹㅍ", "ㄹㅎ", "ㅂㅅ"],
-      mComboIndex: ['ㅘ', 'ㅙ', 'ㅚ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅢ'],
-      mCombo: ['ㅗㅏ', 'ㅗㅐ', 'ㅗㅣ', 'ㅜㅓ', 'ㅜㅔ', 'ㅜㅣ', 'ㅡㅣ'],
-      keys: ['ㄱ','ㄲ','ㄴ','ㄷ','ㄸ','ㄹ','ㅁ','ㅂ','ㅃ','ㅅ','ㅆ','ㅇ','ㅈ','ㅉ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ','ㅏ','ㅐ','ㅑ','ㅒ','ㅓ','ㅔ','ㅕ','ㅖ','ㅗ','ㅘ','ㅙ','ㅚ','ㅛ','ㅜ','ㅝ','ㅞ','ㅟ','ㅠ','ㅡ','ㅢ','ㅣ'],
-      addressInfos: [
-        { label: '도로명 + 건물명', example: '예)서초대로' },
-        { label: '동/읍/면/리 + 번지', example: '예)성수동 999-999' },
-        { label: '건물명/아파트명', example: '예)성수한라시트마벨리' }
-      ]
-    };
-  },
-  methods: {
-    handleKeyDown(event) {
-      const key = event.key;
-      let inputText = this.inputText;
-
-      if (key === ' ') {
-        inputText += ' ';
-        this.inputText = inputText;
-        event.preventDefault();
-        return;
-      } else if (key === 'Backspace') {
-        this.inputText = inputText.slice(0, -1);
-        event.preventDefault();
-        return;
-      }
-
-      let hangeul = '';
-
-      if (/^[ㄱ-ㅎ가-힣]$/.test(key)) {
-        const charCode = key.charCodeAt(0) - 44032;
-        const jongIndex = charCode % 28;
-        const choIndex = parseInt((charCode - jongIndex) / 28 / 21);
-        const chosung = this.chosungIndex[choIndex];
-
-        if (inputText !== '') {
-          const lastChar = inputText.charAt(inputText.length - 1);
-          const lastCharCode = lastChar.charCodeAt(0) - 44032;
-          const lastJongIndex = lastCharCode % 28;
-
-          if (lastJongIndex !== 0 && choIndex !== 0) {
-            const newJongsungIndex = this.jComboIndex.indexOf(this.jongsungIndex[lastJongIndex] + chosung);
-
-            if (newJongsungIndex !== -1) {
-              hangeul = this.jCombo[newJongsungIndex];
-            }
-          }
-        }
-
-        this.inputText = inputText + hangeul;
-      }
-    }
-  }
-};
-</script>
-
 <style scoped>
+.bottom_content {
+  position: fixed;
+  bottom: 20px;
+  left: 0px;
+  width: 100%;
+}
+.info_row {
+  text-align: left;
+  line-height: 30px;
+  color: #767676;
+}
+.info_row span {
+  margin-left: 10px;
+  font-size: 13px;
+}
+.info_box {
+  background-color: #F5F5F5;
+  margin: 0px 30px;
+  padding: 30px 20px;
+  border-radius: 5px;
+}
+.info_txt {
+  color: #767676;
+  font-size: 14px;
+  text-align: left;
+  margin: 10px 30px;
+}
+.icon_row {
+  display: block;
+  text-align: left;
+  margin: 0 30px 10px;
+}
+.tip {
+  display: inline-block;
+  background-color: #EAF2FF;
+  padding: 3px 8px;
+  color: #2960E8;
+  border-top-left-radius: 6px;
+  border-top-right-radius: 6px;
+  border-bottom-right-radius: 6px;
+}
 .addrpage {
   width: 100%;
-  height: 100%;
-  overflow: hidden;
+  height: 1100px;
+  overflow:hidden;
   background-color: #FFFFFF;
 }
-
-.top_row, .bottom_content {
-  margin: 0 30px;
+.temppage_bottom {
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
+  height: 550px;
 }
-
-.top_btn {
-  float: left;
-  width: 20px;
-  margin: 20px 20px;
+img.logo {
+  margin: 180px 0 20px;
+  width: 25%;
 }
-
-h5, .info_txt {
+h5 {
   color: #2C2C2C;
   font-size: 24px;
   font-weight: 700;
-}
-
-h3 {
-  color: #2C2C2C;
-  font-size: 24px;
-}
-
-.ip_row {
   margin-bottom: 20px;
-  overflow: hidden;
 }
-
-.ip_row div {
-  float: left;
-  width: 100%;
-  border-bottom: 2px solid #E4E4E4;
-}
-
-.ip_row div input {
-  width: 100%;
-  padding: 10px;
-  border: 0px;
-  font-size: 20px;
+.msg {
+  line-height: 30px;
   color: #767676;
-  outline: none;
 }
-
+.msg span {
+  display: block;
+  font-weight: 600;
+}
+.bottom_msg {
+  position: absolute;
+  bottom: 30px;
+  width: calc(100% - 40px);
+  margin: 0 0 0 20px;
+  padding: 10px 0px;
+  background-color: #FFFFFF;
+  border-radius: 10px;
+  color: #767676;
+  line-height: 30px;
+}
+.bottom_msg span {
+  display: block;
+  margin-left: 20px;
+  font-weight: 400;
+  text-align: left;
+  font-size: 13px;
+}
 .key_row {
   overflow: hidden;
 }
-
+.key {
+  display: inline-block;
+}
 .key_btn {
   display: inline-block;
   border: 0px;
@@ -192,73 +197,141 @@ h3 {
   width: 88px;
   height: 85px;
 }
-
 .key_btn img {
   height: 20px;
 }
-
 .key_icon {
   width: 88px;
 }
-
+.key_icon {
+  width: 88px;
+}
 .key_space {
   width: 740px;
   color: #F5F5F5;
 }
-
-.icon_row {
-  display: block;
-  margin: 0 0 10px;
+.top_row {
+  overflow: hidden;
 }
-
-.tip {
-  display: inline-block;
-  background-color: #EAF2FF;
-  padding: 3px 8px;
-  color: #2960E8;
-  border-radius: 6px;
+.top_btn {
+  float: left;
+  width: 20px;
+  margin: 20px 20px;
 }
-
-.info_row {
-  text-align: left;
-  line-height: 30px;
+h5 {
   color: #767676;
+  font-size: 20px;
+  font-weight: 400;
+  margin: 100px 0 10px;
 }
-
-.info_row span {
-  margin-left: 10px;
-  font-size: 13px;
+h3 {
+  color: #2C2C2C;
+  font-size: 24px;
+  margin: 0px 0px 100px;
 }
-
-.info_box {
-  background-color: #F5F5F5;
-  padding: 30px 20px;
-  border-radius: 5px;
+.ip_row {
+  margin: 0 40px 20px;
+  overflow: hidden;
 }
-
-.btn_wrap {
-  margin-top: 20px;
-}
-
-.btn {
-  padding: 10px 20px;
-  background-color: #2960E8;
-  color: #FFFFFF;
-  border: none;
-  border-radius: 5px;
-  text-decoration: none;
-  font-size: 16px;
-  cursor: pointer;
-}
-
-.btn:hover {
-  background-color: #1F4AC6;
-}
-
-.bottom_content {
-  position: fixed;
-  bottom: 20px;
-  left: 0px;
+.ip_row div {
+  float: left;
   width: 100%;
+  border-bottom: 2px solid #E4E4E4;
+}
+.ip_row div input {
+  width: 100%;
+  padding: 10px;
+  border: 0px;
+  font-size: 20px;
+  color: #767676;
+  outline: none;
 }
 </style>
+
+<script>
+export default {
+  name: 'AddrPage',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      addr: '',
+      inputText: '', // 사용자가 입력한 텍스트
+      chosungIndex: ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"],
+      jongsungIndex: ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"],
+      jComboIndex: ["ㄳ", "ㄵ", "ㄶ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅄ"],
+      jCombo: ["ㄱㅅ", "ㄴㅈ", "ㄴㅎ", "ㄹㄱ", "ㄹㅁ", "ㄹㅂ", "ㄹㅅ", "ㄹㅌ", "ㄹㅍ", "ㄹㅎ", "ㅂㅅ"],
+      mComboIndex: ['ㅘ', 'ㅙ', 'ㅚ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅢ'],
+      mCombo: ['ㅗㅏ', 'ㅗㅐ', 'ㅗㅣ', 'ㅜㅓ', 'ㅜㅔ', 'ㅜㅣ', 'ㅡㅣ']
+    };
+  },
+  computed: {
+    keys() {
+      return ['ㄱ','ㄲ','ㄴ','ㄷ','ㄸ','ㄹ','ㅁ','ㅂ','ㅃ','ㅅ','ㅆ','ㅇ','ㅈ','ㅉ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ','ㅏ','ㅐ','ㅑ','ㅒ','ㅓ','ㅔ','ㅕ','ㅖ','ㅗ','ㅘ','ㅙ','ㅚ','ㅛ','ㅜ','ㅝ','ㅞ','ㅟ','ㅠ','ㅡ','ㅢ','ㅣ'];
+    }
+  },
+  methods: {
+    handleKeyDown(event) {
+      const key = event.key;
+      let inputText = this.inputText;
+
+      // 스페이스 및 백스페이스는 별도로 처리
+      if (key === ' ') {
+        inputText += ' ';
+        this.inputText = inputText;
+        event.preventDefault();
+        return;
+      } else if (key === 'Backspace') {
+        this.inputText = inputText.slice(0, -1);
+        event.preventDefault();
+        return;
+      }
+
+      // 다른 키는 별도의 로직으로 처리
+      let hangeul = '';
+
+      // 한글 자소 분해 로직
+      // Unicode 기준 초성, 중성, 종성을 분리
+      if (/^[ㄱ-ㅎ가-힣]$/.test(key)) {
+        const charCode = key.charCodeAt(0) - 44032;
+        const jongIndex = charCode % 28;
+        const choIndex = parseInt((charCode - jongIndex) / 28 / 21);
+
+        const chosung = this.chosungIndex[choIndex];
+
+        // 이전 글자와 조합된 자음 또는 모음일 경우 처리
+        // ex) ㄱ + ㅏ = 가, ㄱ + ㄱ = ㄲ
+        // (이 부분의 로직은 한글 조합 규칙에 따라 작성되어야 합니다.)
+        // 이 부분의 로직을 통해 입력된 한글을 올바르게 조합하여 hangeul에 저장
+        if (inputText !== '') {
+          const lastChar = inputText.charAt(inputText.length - 1);
+          const lastCharCode = lastChar.charCodeAt(0) - 44032;
+          const lastJongIndex = lastCharCode % 28;
+          //const lastChoIndex = parseInt((lastCharCode - lastJongIndex) / 28 / 21);
+          //const lastChosung = this.chosungIndex[lastChoIndex];
+
+          // 이전 글자의 종성이 있고, 현재 글자가 초성일 경우
+          if (lastJongIndex !== 0 && choIndex !== 0) {
+            // 이전 글자의 종성과 현재 글자의 초성으로 새로운 종성을 구함
+            const newJongsungIndex = this.jComboIndex.indexOf(this.jongsungIndex[lastJongIndex] + chosung);
+            // 새로운 종성이 존재한다면
+            if (newJongsungIndex !== -1) {
+              // 종성을 변경
+              hangeul = this.jCombo[newJongsungIndex];
+            }
+          }
+        }
+
+        // 최종 hangeul을 inputText에 추가
+        this.inputText = inputText + hangeul;
+      } else {
+        // 특수문자 또는 알파벳 등의 경우 처리할 로직 추가
+        // 여기에 추가적인 로직을 작성하세요
+      }
+    }
+  }
+};
+</script>
+
+
